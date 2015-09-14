83a8d0981c256e0c2269e8c66191cb988c277665
starkhorn <t.earendel@gmail.com>
2015-09-15 00:24:10 +0700

    * Persist current question when in a saved state to restore its value upon rotating the device

--- app/src/main/java/com/example/starkhorn/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/starkhorn/geoquiz/QuizActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -78,6 +79,10 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         updateQuestion();
     }
 
@@ -98,6 +103,14 @@ public class QuizActivity extends AppCompatActivity {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
 
         mQuestionTextView.setText(question);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     @Override
